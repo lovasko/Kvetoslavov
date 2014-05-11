b6332e87908778b7b2fd0914a78d463d2e3266b6
Daniel Lovasko <daniel.lovasko@gmail.com>
2014-05-11 22:19:50 +0200

    Line parsing utils

---
+++ src/line.c
@@ -0,0 +1,72 @@
+#include "line.h"
+
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+unsigned int
+line_argument_count (char *line, char delimiter)
+{
+	unsigned int i, count;
+	size_t line_size;
+
+	count = 0;
+	for	(i = 0; line[i]; i++)
+	{
+		if (line[i] == delimiter)	
+		{
+			if (line[i+1] == line[i] || line[i+1] == '\0')
+				continue;
+
+			count++;
+		}
+	}
+
+	return count + 1;
+}
+
+char**
+line_get_arguments (char **line, char delimiter)
+{
+	char **result;
+	char *token;
+	unsigned int index;
+	
+	result = (char**)malloc(sizeof(char*) * line_argument_count(*line,
+	    delimiter));	
+	index = 0;
+
+	while ((token = strsep(line, &delimiter)) != NULL)	
+
+	{
+		if (token[0] == '\0')
+			continue;
+		
+		result[index++] = token;
+	}
+
+	return result;
+}
+
+char*
+line_read_stdin (unsigned int max_length)
+{
+	char *line;
+	int i;
+	char c;
+	
+	/* this malloc should be freed by the caller */
+	line = (char*)malloc(sizeof(char) * max_length);
+	memset(line, 0, max_length);
+	
+	/* upper limit is max_lenght - 1 to ensure NULL ending */
+	for (i = 0; i < max_length - 1; i++) 
+	{ 
+		read(0, &c, 1);
+		if(c == '\n') break;
+		
+		line[i] = c;
+	}
+	
+	return line;
+}
---
+++ src/line.h
@@ -0,0 +1,33 @@
+#ifndef LINE_H
+#define LINE_H
+
+/**
+ * Counts the number of delimiters in the line. 
+ * Consecutive delimiters are treated as one.
+ * @param line the text to be searched
+ * @param delimiter the delimiter
+ * @return number of delimiters in the text
+ */
+unsigned int
+line_argument_count (char *line, char delimiter);
+
+/**
+ * Accept bytes from standard input, until end of line or max_length is reached.
+ * @param max_length maximum length of line in bytes, including terminating NULL
+ * @return NULL-terminated array of char
+ */
+char*
+line_read_stdin (unsigned int max_length);
+
+/**
+ * Returns array of strings.
+ * Consecutive delimiters are treated as one.
+ * @param line the text
+ * @param delimiter the delimiter
+ * @return array of char pointers 
+ */
+char**
+line_get_arguments (char **line, char delimiter);
+
+#endif
+
