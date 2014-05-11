c0698e23942f632b6fb04b192210daafe01d92a7
Daniel Lovasko <daniel.lovasko@gmail.com>
2014-05-11 22:36:43 +0200

    Command utility functions

---
+++ src/command.c
@@ -0,0 +1,31 @@
+#include "command.h"
+#include "state.h"
+#include "line.h"
+
+void
+command_print_compatible_states (struct command_t *command)
+{
+	if (state_is_compatible(DEFAULT, command))   
+		fprintf(stderr, "\tdef\n");
+
+	if (state_is_compatible(PREPARING, command))   
+		fprintf(stderr, "\tprep\n");
+
+	if (state_is_compatible(RUNNING, command))   
+		fprintf(stderr, "\trun\n");
+
+	fprintf(stderr, "\n");
+}
+
+struct command_t*
+command_match (struct command_t *commands, char *line)
+{
+	unsigned int i;
+
+	for (i = 0; commands[i].name != NULL; i++)
+		if (line_starts_with(line, commands[i].name) == 0)
+			return &commands[i];
+
+	return NULL;
+}
+
---
+++ src/command.h
@@ -0,0 +1,38 @@
+#ifndef COMMAND_H
+#define COMMAND_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct command_args_t
+{
+	pid_t *pid;
+	int *state;
+};
+
+struct command_t
+{
+	const char *name;
+	int (*function)(struct command_args_t *args);
+	int compatible_state;
+	unsigned short expected_arg_count;
+};
+
+/**
+ * Print all compatible state names to stdout.
+ * @param command command
+ */
+void
+command_print_compatible_states (struct command_t *command);
+
+/**
+ * Find command by matching its name with the string.
+ * @param commands pointer to array of commands
+ * @param line string
+ * @return pointer to one of the array elements or NULL on failed match
+ */
+struct command_t*
+command_match (struct command_t* commands, char *line);
+
+#endif
+
