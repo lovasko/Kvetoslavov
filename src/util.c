5f88abff454c65658a83b931c762dfe7acca90a3
Daniel Lovasko <daniel.lovasko@gmail.com>
2014-05-12 18:32:55 +0100

    Added new functions to the util

--- src/util.c
+++ src/util.c
@@ -1,6 +1,9 @@
 #include "util.h"
 
 #include <signal.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 void ignore_sigint()
 {
@@ -15,5 +18,39 @@ void ignore_sigint()
 	{
 		sigaction(SIGINT, &new_sa, 0);
 	}
+}
+
+int 
+file_exists (char *path)
+{
+	int fd;
+ 
+	fd = open(path, O_RDONLY);
+	close(fd);
+	
+	if (fd < 0) 
+		return 1;
+	else 
+		return 0;
+}
+
+int 
+is_elf (char *path)
+{
+	int fd;
+	char magic[4]; 
+	
+	fd = open(path, O_RDONLY);
+    
+	/* test the beginning of file for ELF magic number */
+	memset(magic, 0, 4);
+	read(fd, &magic, 4);
+
+	close(fd);
+
+	if (strncmp(magic, "\177ELF", 4) == 0)
+		return 0;
+	else
+		return 1;
 }
 
--- src/util.h
+++ src/util.h
@@ -6,5 +6,21 @@
  */
 void ignore_sigint();
 
+/**
+ * Check whether file exists.
+ * @param path to file
+ * @return 1 if file exists, 0 otherwise
+ */
+int 
+file_exists (char *path);
+
+/**
+ * Check whether file is an ELF binary.
+ * @param path to file
+ * @return 0 if file is an ELF binary, 1 otherwise
+ */  
+int 
+is_elf (char *path);
+
 #endif
 
