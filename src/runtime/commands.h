172f6418fd4b538f94e5c94006fa9a681bb0e47a
Daniel Lovasko <daniel.lovasko@gmail.com>
2014-05-13 03:07:35 +0100

    Added run and attach to the registred commands

--- src/runtime/commands.h
+++ src/runtime/commands.h
@@ -7,6 +7,8 @@
 #include "runtime/general/exit.h"
 #include "runtime/general/select.h"
 #include "runtime/general/status.h"
+#include "runtime/control/run.h"
+#include "runtime/control/attach.h"
 
 struct command_t registred_commands[] =
 {
@@ -17,6 +19,8 @@ struct command_t registred_commands[] =
 	{"select", runtime_command_select, DEFAULT, 2},
 	{"status", runtime_command_status, ALL_STATES, 1},
 	{"info", runtime_command_status, ALL_STATES, 1},
+	{"run", runtime_command_run, PREPARING, 1},
+	{"attach", runtime_command_attach, DEFAULT, 2},
 	NULL
 };
 
