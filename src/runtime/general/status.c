ca0253e7cc37830e7750ac966544094db480ea45
Daniel Lovasko <daniel.lovasko@gmail.com>
2014-05-13 03:05:04 +0100

    PID support in the status command

--- src/runtime/general/status.c
+++ src/runtime/general/status.c
@@ -6,8 +6,10 @@
 int 
 runtime_command_status (struct command_args_t *args)
 {
+	fprintf(stdout, "            PID: %d\n", *(args->pid));
 	fprintf(stdout, "          State: %s\n", state_to_string(*(args->state)));
 	fprintf(stdout, "Executable path: %s\n", *(args->exec_path));
 
 	return 0;
 }
+
