6d75e11e6e8815d2917d4625fb427962ba4b3be1
Daniel Lovasko <daniel.lovasko@gmail.com>
2014-05-14 14:40:30 +0100

    Aligned part of the table

--- src/runtime/commands.h
+++ src/runtime/commands.h
@@ -15,17 +15,17 @@
 
 struct command_t registred_commands[] =
 {
-	{"?",    runtime_command_help, ALL_STATES, 1},
-	{"help", runtime_command_help, ALL_STATES, 1},
-	{"exit", runtime_command_exit, ALL_STATES, 1},
-	{"quit", runtime_command_exit, ALL_STATES, 1},
-	{"select", runtime_command_select, DEFAULT, 2},
-	{"status", runtime_command_status, ALL_STATES, 1},
-	{"info", runtime_command_status, ALL_STATES, 1},
-	{"run", runtime_command_run, PREPARING, 1},
-	{"attach", runtime_command_attach, DEFAULT, 2},
-	{"detach", runtime_command_detach, RUNNING, 1},
-	{"stop", runtime_command_stop, RUNNING, 1},
+	{"?",        runtime_command_help,     ALL_STATES, 1},
+	{"help",     runtime_command_help,     ALL_STATES, 1},
+	{"exit",     runtime_command_exit,     ALL_STATES, 1},
+	{"quit",     runtime_command_exit,     ALL_STATES, 1},
+	{"select",   runtime_command_select,   DEFAULT, 2},
+	{"status",   runtime_command_status,   ALL_STATES, 1},
+	{"info",     runtime_command_status,   ALL_STATES, 1},
+	{"run",      runtime_command_run,      PREPARING, 1},
+	{"attach",   runtime_command_attach,   DEFAULT, 2},
+	{"detach",   runtime_command_detach,   RUNNING, 1},
+	{"stop",     runtime_command_stop,     RUNNING, 1},
 	{"continue", runtime_command_continue, RUNNING, 1},
 	NULL
 };
