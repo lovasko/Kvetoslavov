32ca063feba5d1955fd45c33572e7147f1b9e4d1
Daniel Lovasko <daniel.lovasko@gmail.com>
2014-05-17 01:12:30 +0100

    Added breakpoint command and reorganized the table

--- src/runtime/commands.h
+++ src/runtime/commands.h
@@ -12,21 +12,24 @@
 #include "runtime/control/detach.h"
 #include "runtime/control/stop.h"
 #include "runtime/control/continue.h"
+#include "runtime/breakpoint/master.h"
 
 struct command_t registred_commands[] =
 {
-	{"?",        runtime_command_help,     ALL_STATES, 0},
-	{"help",     runtime_command_help,     ALL_STATES, -1},
-	{"exit",     runtime_command_exit,     ALL_STATES, 0},
-	{"quit",     runtime_command_exit,     ALL_STATES, 0},
-	{"select",   runtime_command_select,   DEFAULT, 1},
-	{"status",   runtime_command_status,   ALL_STATES, 0},
-	{"info",     runtime_command_status,   ALL_STATES, 0},
-	{"run",      runtime_command_run,      PREPARING, 0},
-	{"attach",   runtime_command_attach,   DEFAULT, 1},
-	{"detach",   runtime_command_detach,   RUNNING, 0},
-	{"stop",     runtime_command_stop,     RUNNING, 0},
-	{"continue", runtime_command_continue, RUNNING, 0},
+	{"?",          runtime_command_help,              ALL_STATES, 0},
+	{"help",       runtime_command_help,              ALL_STATES, -1},
+	{"exit",       runtime_command_exit,              ALL_STATES, 0},
+	{"quit",       runtime_command_exit,              ALL_STATES, 0},
+	{"select",     runtime_command_select,            DEFAULT,    1},
+	{"status",     runtime_command_status,            ALL_STATES, 0},
+	{"info",       runtime_command_status,            ALL_STATES, 0},
+	{"run",        runtime_command_run,               PREPARING,  0},
+	{"attach",     runtime_command_attach,            DEFAULT,    1},
+	{"detach",     runtime_command_detach,            RUNNING,    0},
+	{"stop",       runtime_command_stop,              RUNNING,    0},
+	{"continue",   runtime_command_continue,          RUNNING,    0},
+	{"breakpoint", runtime_command_breakpoint_master, 0,         -1},
+	{"bp",         runtime_command_breakpoint_master, 0,         -1},
 	NULL
 };
 
