1c981c93859ee1bf8654097896551ce3c2043368
Daniel Lovasko <lovasko@freebsd.org>
2015-03-09 03:29:13 +0100

    Minimal command list

--- src/runtime/commands.h
+++ src/runtime/commands.h
@@ -16,21 +16,21 @@
 
 struct command registred_commands[] =
 {
+	{"exit",       runtime_command_exit,              ALL_STATES, 0},
+/*
 	{"?",          runtime_command_help,              ALL_STATES, 0},
 	{"attach",     runtime_command_attach,            DEFAULT,    1},
 	{"bp",         runtime_command_breakpoint_master, 0,         -1},
 	{"breakpoint", runtime_command_breakpoint_master, 0,         -1},
 	{"continue",   runtime_command_continue,          RUNNING,    0},
 	{"detach",     runtime_command_detach,            RUNNING,    0},
-	{"exit",       runtime_command_exit,              ALL_STATES, 0},
 	{"help",       runtime_command_help,              ALL_STATES, -1},
 	{"info",       runtime_command_status,            ALL_STATES, 0},
 	{"quit",       runtime_command_exit,              ALL_STATES, 0},
 	{"run",        runtime_command_run,               PREPARING,  0},
 	{"select",     runtime_command_select,            DEFAULT,    1},
 	{"status",     runtime_command_status,            ALL_STATES, 0},
-	{"stop",       runtime_command_stop,              RUNNING,    0},
-	NULL
+	{"stop",       runtime_command_stop,              RUNNING,    0}, */
 };
 
 #endif
