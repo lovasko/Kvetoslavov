846ba8b15df88e5af513be1b160dc9c3affb6fcb
Daniel Lovasko <lovasko@freebsd.org>
2015-03-09 03:04:12 +0100

    Fixed renaming issues

--- src/command/command.c
+++ src/command/command.c
@@ -7,13 +7,13 @@
 void
 command_print_compatible_states(struct command* cmd)
 {
-	if (state_is_compatible(DEFAULT, command))   
+	if (state_is_compatible(DEFAULT, cmd))
 		fprintf(stderr, "\tdef\n");
 
-	if (state_is_compatible(PREPARING, command))   
+	if (state_is_compatible(PREPARING, cmd))
 		fprintf(stderr, "\tprp\n");
 
-	if (state_is_compatible(RUNNING, command))   
+	if (state_is_compatible(RUNNING, cmd))
 		fprintf(stderr, "\trun\n");
 }
 
@@ -22,9 +22,9 @@ command_match(struct command* cmds, unsigned int n_cmds, char* line)
 {
 	unsigned int i;
 
-	for (i = 0; commands[i].name != NULL && i < n_cmds; i++)
-		if (line_starts_with(line, commands[i].name) == 0)
-			return &commands[i];
+	for (i = 0; cmds[i].name != NULL && i < n_cmds; i++)
+		if (line_starts_with(line, cmds[i].name) == 0)
+			return &cmds[i];
 
 	return NULL;
 }
