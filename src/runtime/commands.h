734cd3c78533fda59e72aadb5470fcd2a9069811
Daniel Lovasko <lovasko@freebsd.org>
2015-03-09 04:02:29 +0100

    Re-added status/info command

--- src/runtime/commands.h
+++ src/runtime/commands.h
@@ -19,17 +19,17 @@ struct command registred_commands[] =
 	{"?",          runtime_command_help,              ALL_STATES, -1},
 	{"exit",       runtime_command_exit,              ALL_STATES, 0},
 	{"help",       runtime_command_help,              ALL_STATES, -1},
+	{"info",       runtime_command_status,            RUNNING,    0},
 	{"quit",       runtime_command_exit,              ALL_STATES, 0},
+	{"status",     runtime_command_status,            RUNNING,    0},
 /*
 	{"attach",     runtime_command_attach,            DEFAULT,    1},
 	{"bp",         runtime_command_breakpoint_master, 0,         -1},
 	{"breakpoint", runtime_command_breakpoint_master, 0,         -1},
 	{"continue",   runtime_command_continue,          RUNNING,    0},
 	{"detach",     runtime_command_detach,            RUNNING,    0},
-	{"info",       runtime_command_status,            ALL_STATES, 0},
 	{"run",        runtime_command_run,               PREPARING,  0},
 	{"select",     runtime_command_select,            DEFAULT,    1},
-	{"status",     runtime_command_status,            ALL_STATES, 0},
 	{"stop",       runtime_command_stop,              RUNNING,    0}, */
 };
 
