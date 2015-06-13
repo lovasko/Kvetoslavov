63a355611efa91bcabb87ad18610867face04f9c
Daniel Lovasko <lovasko@freebsd.org>
2015-06-13 08:16:16 +0100

    simplified command

--- src/runtime/command.h
+++ src/runtime/command.h
@@ -1,46 +1,15 @@
-#ifndef KVETOSLAVOV_COMMAND_COMMAND_H
-#define KVETOSLAVOV_COMMAND_COMMAND_H
+#ifndef KVETOSLAVOV_RUNTIME_COMMAND_H
+#define KVETOSLAVOV_RUNTIME_COMMAND_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-
-#include "runtime/breakpoint/breakpoint.h"
-
-struct command_args
-{
-	int* state;
-	char** text_args;
-	char** exec_path;
-	pid_t* pid;
-	struct breakpoint** bp_head;
-	struct breakpoint** bp_head_add;
-	struct breakpoint** bp_head_remove;
-};
+#include "runtime/context.h"
 
 struct command
 {
 	const char* name;
-	int (*function)(struct command_args* args);
-	int compatible_state;
-	int expected_arg_count;
+	int (*fn)(struct context* ctx);
+	int state_flags;
+	int arg_count;
 };
-
-/**
- * Print all compatible state names to stdout.
- * @param command command
- */
-void
-command_print_compatible_states(struct command* cmd);
-
-/**
- * Find command by matching its name with the string.
- * @param commands pointer to array of commands
- * @param line string
- * @return pointer to one of the array elements or NULL on failed match
- */
-struct command*
-command_match(struct command* cmds, unsigned int n_cmds, char* line);
 
 #endif
 
