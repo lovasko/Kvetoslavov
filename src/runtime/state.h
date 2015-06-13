5d32322f4863dbe12eebc1b8d8c8b1d4e18af02a
Daniel Lovasko <lovasko@freebsd.org>
2015-06-13 08:12:45 +0100

    simplified state and added prefix

--- src/runtime/state.h
+++ src/runtime/state.h
@@ -1,36 +1,10 @@
-#ifndef KVETOSLAVOV_STATE_STATE_H
-#define KVETOSLAVOV_STATE_STATE_H
+#ifndef KVETOSLAVOV_RUNTIME_STATE_H
+#define KVETOSLAVOV_RUNTIME_STATE_H
 
-#include "command/command.h"
-
-#define DEFAULT    1
-#define PREPARING  2
-#define RUNNING    4
-#define ALL_STATES 7
-
-/**
- * Inform user about the debugging phase we are in.
- * @param state the state to be printed
- */
-void 
-state_print(int state);
-
-/**
- * Check if the state is compatible.
- * @param check 
- * @param command 
- * @return 1 if command is compatible with check, 0 otherwise
- */
-int 
-state_is_compatible(int state, struct command* cmd);
-
-/**
- * Converts the state to a string representation.
- * @param state the state
- * @return string representation of the state.
- */
-const char*
-state_to_string(int state);
+#define STATE_DEFAULT    1
+#define STATE_PREPARING  2
+#define STATE_RUNNING    4
+#define STATE_ALL        7
 
 #endif
 
